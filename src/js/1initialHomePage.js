0063226ec1631aa12247a2f069101473f74b80af
Alex <savchukalex57@gmail.com>
2019-07-06 20:14:13 +0300

    init homePage_alex_branch

---
+++ src/js/1initialHomePage.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+const jsList = document.querySelector('#js-list');
+let renderFilms;
+let pageNumber = 1;
+let ganre;
+jsList.addEventListener('click', activeDetailsPage);
+
+window.onload = fetchPopularMoviesList();
+
+let selectFilm = {};
+
+
+function activeDetailsPage(e) {
+    selectFilm.id = e.target.getAttribute('alt');
+ 
+
+}
+function fetchPopularMoviesList() {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=8498946f9c7874ef33ac19a931c494c9&language=en-US&page=' + `${pageNumber}`)
+        .then(response => {
+            if (response.ok) return response.json();
+
+            throw new Error(`Error while fetching: ${response.statusText}`);
+        })
+        .then(data => {
+            
+
+            let result = data.results;
+            result.forEach(element => {
+                createCardFunc(element.poster_path, element.title, element.id);
+
+            });
+
+
+
+        })
+        .catch(error => console.log(error));
+}
+
+
+function createCardFunc(imgPath, filmTitle, movieId) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    img.setAttribute('src', `https://image.tmdb.org/t/p/w500` + `${imgPath}`);
+    img.setAttribute('alt', `${movieId}`);
+    p.textContent = `${filmTitle}`;
+    li.append(img);
+    li.append(p);
+    jsList.append(li);
+    img.classList.add('homePage__img');
+    li.classList.add('homePage__items');
+    p.classList.add('homePage__text'); 
+}
